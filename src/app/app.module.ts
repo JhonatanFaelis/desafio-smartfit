63ffda6a27fe7bff96946bd3903f3cafaafc900c
SmartCrm <nossocrmimob@gmail.com>
2023-10-09 20:40:38 -0300

    form-component and footer-component - frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
