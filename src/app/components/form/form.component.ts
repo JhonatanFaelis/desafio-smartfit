78e32396878dff00560d39100b9f08bcd0e77fcc
SmartCrm <nossocrmimob@gmail.com>
2023-10-10 18:58:23 -0300

    search filter correction

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,28 +1,11 @@
 import { LocationStrategy } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { FilterUnitsService } from 'src/app/services/filter-units.service';
 import { UnitsService } from 'src/app/services/units.service';
 import { Location } from 'src/app/types/location.interface';
 import {UnitsResponse} from 'src/app/types/units-response.interface'
 
-
-const HOURS = {
-  morning: {
-    first: '06',
-    last:'12'
-  } ,
-  after :{
-    first: '12',
-    last:'18'
-  },
-  night : {
-    first: '18',
-    last:'23'
-  }
-
-}
-
-type HOUR_INDEX = 'morning' | 'after' | 'night';
 
 @Component({
   selector: 'app-form',
@@ -30,94 +13,40 @@ type HOUR_INDEX = 'morning' | 'after' | 'night';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit{
-  results : Location[] = [];
-  filterResults : Location[]  = [];
+  results : Location[]  = []
+  filteredResults : Location[]  = [];
   formGroup! : FormGroup;
 
-  constructor( 
+  constructor(
     private formBuilder : FormBuilder,
-    private unitService : UnitsService
-    
+    private unitService : UnitsService,
+    private filterUnitsService: FilterUnitsService
+
     ){}
 
 
-  ngOnInit():void{
-    
-    this.formGroup = this.formBuilder.group({
-      hour: '',
-      showClosed: false
-    })
-    this.unitService.getAllUnits().subscribe(data => {
-      this.results = data.locations;
-      this.filterResults = data.locations;
-    })
-  }
-
-  filterUnits(unit:Location, open_hour:string, close_hour:string){
-    debugger
-    if(!unit.schedules) return true;
-    let open_hour_filter = parseInt(open_hour,10);
-    let close_hour_hour_filter = parseInt(close_hour,10);
-
-    let today_weekday = this.transformWeekDay(new Date().getDay());
-
-    for (let i = 0; i < unit.schedules.length; i++) {
-      let schedule_hour = unit.schedules[i].hour;
-      let schedule_weekday = unit.schedules[i].weekdays;
-
-      if (today_weekday == schedule_weekday) {
-        if (schedule_hour != 'Fechada') {
-          let [unit_open_hour, unit_close_hour] = schedule_hour.split(' ás ');
-          let unit_open_hour_int = parseInt(unit_open_hour.replace('h', ''), 10) 
-          let unit_close_hour_int = parseInt(unit_close_hour.replace('h', ''), 10)
-
-          if(unit_open_hour_int <= open_hour_filter && unit_close_hour_int >= close_hour_hour_filter) return true
-          else return false
-        } 
-      }   
+    ngOnInit(): void {
+      this.formGroup = this.formBuilder.group({
+        hour: '',
+        showClosed: true
+      })
+      this.unitService.getAllUnits().subscribe(data => {
+        this.results = data;
+        this.filteredResults = data;
+      });
     }
-    return false;
-  }
 
-  onClear(): void{
-    this.formGroup.reset();
-  }
+    onSubmit(): void {
+      let { showClosed, hour } = this.formGroup.value
+      this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour);
+      this.unitService.setFilteredUnits(this.filteredResults);
 
-  onSubmit(): void{
-    debugger
-    let intermediateResults = this.results;
-    if (!this.formGroup.value.showClosed) {
-      intermediateResults = this.results.filter(location => location.opened == true)     
+      // this.submitEvent.emit();
     }
-   
-    if (this.formGroup.value.hour) {
-      const OPEN_HOUR = HOURS[this.formGroup.value.hour as HOUR_INDEX].first
-      const CLOSE_HOUR = HOURS[this.formGroup.value.hour as HOUR_INDEX].last
-      this.filterResults = intermediateResults.filter(location => this.filterUnits(location,OPEN_HOUR,CLOSE_HOUR))
+
+    onClean(): void {
+      this.formGroup.reset();
     }
-    else{
-      this.filterResults = intermediateResults;
-    }
-  
-
-    
-  }
-
-
-  transformWeekDay(weekday : number){
-    switch (weekday) {
-      case 0:
-        return 'Dom.'
-        break;
-        case 6:
-        return 'Sab.'
-        break;
-      default:
-        return 'Seg. a Sex.'
-        break;
-    }
-  }
-
 
 
 }
--- src/app/services/units.service.ts
+++ src/app/services/units.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { UnitsResponse } from '../types/units-response.interface';
+import {Location} from '../types/location.interface'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UnitsService {
 
-  constructor(private http : HttpClient) { }
-
-
+  
   readonly urlApi =  'https://test-frontend-developer.s3.amazonaws.com/data/locations.json';
 
-  getAllUnits():Observable<UnitsResponse>{
-    return this.http.get<UnitsResponse>(this.urlApi);
+  private allUnitsSubject: BehaviorSubject<Location[]> = new BehaviorSubject<Location[]>([]);
+  private allUnits$: Observable<Location[]> = this.allUnitsSubject.asObservable();
+  private filteredUnits: Location[] = [];
+
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get<UnitsResponse>(this.urlApi).subscribe(data => {
+      this.allUnitsSubject.next(data.locations);
+      this.filteredUnits = data.locations;
+    });
+  }
+
+  getAllUnits(): Observable<Location[]> {
+    return this.allUnits$;
+  }
+
+  getFilteredUnits() {
+    return this.filteredUnits;
+  }
+
+  setFilteredUnits(value: Location[]) {
+    this.filteredUnits = value;
   }
 }
